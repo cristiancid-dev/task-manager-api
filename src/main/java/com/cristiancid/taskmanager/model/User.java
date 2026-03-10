76359a799549eb5ca8a69976be2c7ee4eab74983
Cristian <ccidbe@gmail.com>
2026-03-10 23:23:11 +0100

    Add User Entity

--- src/main/java/com/cristiancid/taskmanager/model/User.java
+++ src/main/java/com/cristiancid/taskmanager/model/User.java
@@ -1,15 +1,45 @@
 package com.cristiancid.taskmanager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-class User {
+@Table(name = "users")
+public class User {
 
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    protected User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
