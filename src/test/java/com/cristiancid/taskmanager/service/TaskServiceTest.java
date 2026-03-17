00112fbd1cc098d6f53669f7361f6e3f2c822694
Cristian <ccidbe@gmail.com>
2026-03-17 19:29:38 +0100

    test: add unit tests for TaskService.completeTask

---
+++ src/test/java/com/cristiancid/taskmanager/service/TaskServiceTest.java
@@ -0,0 +1,54 @@
+package com.cristiancid.taskmanager.service;
+
+import com.cristiancid.taskmanager.exception.TaskNotFoundException;
+import com.cristiancid.taskmanager.model.Task;
+import com.cristiancid.taskmanager.model.User;
+import com.cristiancid.taskmanager.repository.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+    @Mock
+    TaskRepository taskRepository;
+
+    @InjectMocks
+    TaskService taskService;
+
+    @Test
+    void shouldMarkTaskAsCompletedWhenTaskExists() {
+        Long id = 1L;
+        User user = new User("Cristian", "ccidbe@gmail.com");
+        Task task = new Task("task 1", false, user);
+        when(taskRepository.findById(id)).thenReturn(Optional.of(task));
+        when(taskRepository.save(task)).thenReturn(task);
+
+        Task result = taskService.completeTask(id);
+
+        assertTrue(result.isCompleted());
+        verify(taskRepository).findById(id);
+        verify(taskRepository).save(task);
+    }
+
+    @Test
+    void shouldThrowTaskNotFoundExceptionWhenTaskDoesNotExist() {
+        Long id = 1L;
+        when(taskRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(TaskNotFoundException.class, () -> {
+            taskService.completeTask(id);
+        });
+        verify(taskRepository).findById(id);
+        verify(taskRepository, never()).save(any(Task.class));
+
+    }
+
+}
